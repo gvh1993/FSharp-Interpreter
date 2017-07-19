ade19da925d9639ca61c93d154ad7dc58a385d19
gvh1993 <gertjanvanh@live.nl>
2017-07-19 13:40:33 +0200

    init commit

---
+++ fSharpApplication/Interpreter.fs
@@ -0,0 +1,19 @@
+﻿module Interpreter
+// Declerations
+type Decleration = {
+    Type: int
+
+}
+let Declerate (typeValue : 'a) (name : 'b) =
+    
+// printfn
+
+// Arithmetic operators
+// TODO add, minus, multiplication, dividing, modulus
+
+// Logical operators
+// TODO equals, greater than, less than, not equals, OR, AND
+
+// TODO if, else
+
+// TODO while, for
---
+++ fSharpApplication/Program.fs
@@ -0,0 +1,11 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open Interpreter
+
+[<EntryPoint>]
+let main argv = 
+    
+
+//    System.Console.WriteLine(Client.lucky)
+    System.Console.Read()
+    0 // return an integer exit code
