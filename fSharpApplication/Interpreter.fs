236af5ef959e9cba9064e53a929e5ce6d332905a
gvh1993 <gertjanvanh@live.nl>
2017-08-02 23:26:55 +0200

    made begin

--- fSharpApplication/Interpreter.fs
+++ fSharpApplication/Interpreter.fs
@@ -1,19 +1,21 @@
 ﻿module Interpreter
-// Declerations
-type Decleration = {
-    Type: int
 
-}
-let Declerate (typeValue : 'a) (name : 'b) =
-    
-// printfn
+open Structure
 
-// Arithmetic operators
-// TODO add, minus, multiplication, dividing, modulus
+exception Exception of string
 
-// Logical operators
-// TODO equals, greater than, less than, not equals, OR, AND
+//let rec evaluateExpression variables expression =
+//    match expression with
+//    | Value v -> v
+//    | Var id -> 
+//        match Map.tryFind id variables with
+//        | Some value -> value
+//        | None -> raise(Exception("Exceptionnnn!!")) //the raise method creates an exception
+//    | 
 
-// TODO if, else
 
-// TODO while, for
+let rec evaluateStatement variables statement : Map<string, ValueType> =
+    match statement with
+    | Block block ->
+        block |> List.fold(fun updatedVariables statement -> evaluateStatement updatedVariables statement) variables //fold is iterating through the block and executes 'evaluateStatement' and returns variables
+        
---
+++ fSharpApplication/Structure.fs
@@ -0,0 +1,48 @@
+﻿module Structure
+
+// Declerations
+type ValueType =
+    | String of string
+    | Integer of int
+    | Boolean of bool
+    | Float of float
+    | Double of double
+
+//
+//
+//type Decleration = {
+//    Type: Type;
+//    Name: string
+//}
+//
+//let Declerate (typeValue : 'a) (name : 'b) =
+    
+    
+// printfn
+
+
+type Expression = 
+    | Value of ValueType
+    | Var of string
+
+    // Arithmetic operators
+    // add, minus, multiplication, dividing, modulus
+    // * is for creating a tuple
+    | Add of Expression * Expression
+    | Subtract of Expression * Expression
+    | Divide of Expression * Expression
+    | Multiply of Expression * Expression
+    | Modulo of Expression * Expression
+
+// Logical operators
+// TODO equals, greater than, less than, not equals, OR, AND
+
+
+
+// while, for, if, else
+type Statement =
+    | Block of Statement list
+    | If of Expression * Statement
+    | Elif of Expression * Statement * Statement
+    | While of Expression * Statement
+
